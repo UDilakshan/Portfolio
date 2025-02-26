2fa94f238b542b9f07e4b2f73497f9bea7b159ae
UDilakshan <uthayakumardilakshan@gmail.com>
2025-02-26 19:21:20 +0530

    Other changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <>
-     <Navbar />
-     <Hero />
+      <Navbar />
+      <Hero />
+      <Footer/>
+      <Contact />
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,24 +11,31 @@ import reduxLogo from "../assets/Redux.png"
 
 const Hero = () => {
   return (
-    <section className=''>
-          <div className='max-w-7xl mx-auto '>
-              <div className="flex flex-col md:flex-row items-center lg:h-[90vh] justify-between ">
-                <div className="md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0 mt-10">
-                <h1 className='lg:text-7xl text-4xl font-bold lg:leading-snug'>Hi There,
-                  <br />I'm Dilakshan </h1>
-                  <p className='md:text-2xl text-xl mb-4'>Full stack Developer & Designer</p>
-                  <p className='mb-4'>I'm a passionate full stack developer with expertise in React, Next.js and modern web technologies.I love creating beautiful and functional web applications that solve real world problems.</p>
-                  <button className='bg-black text-white px-3 py-2 w-max rounded-md'>Download CV</button>
-                </div>
-                <div className='md:w-1/2 flex justify-center items-end'>
-                <img src={hero} alt="" className='lg:h-[90vh] h-96' />
-                {/* <img src={reactLogo} alt="" /> */}
-                </div>
-              </div>
-    
-          </div>
+        <section className='relative'>
+        <div className='max-w-7xl mx-auto'>
+          <div className='flex flex-col md:flex-row items-center lg:h-[90vh] justify-between' >
+            <div className='md:w-1/2 mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0 mt-10'>
+               <h1 className='lg:text-7xl text-2xl font-bold lg:leading-snug'>Hi There, <br />I'm <span className='text-red-500'>Dilakshan</span></h1>
+               <p className='md:text-2xl font-semibold text-xl mb-4'>Full stack Developer & Designer</p>
+               <p className='mb-4'>I'm a passionate full stack developer with expertise in React, Next.js and modern web technologies.I love creating beautiful and functional web applications that solve real world problems.</p>
+               <button className='bg-black text-white px-3 py-2 w-max rounded-md'><a href="https://drive.google.com/file/d/1LBMHWpTgTGkvCTcHYiU8JQRCHb5-SRl8/view?usp=sharing" download target='_blank'>Download CV</a></button>
+            </div>
+            <div className='md:w-1/2 relative flex justify-center items-end'>
+                 <img src={hero} alt="" className='lg:h-[90vh] h-96' />
+                 <img src={reactLogo} alt="" className='absolute w-10 top-36 left-0 rounded-full md:hidden'/>
+                 <img src={reduxLogo} alt="" className='absolute w-10 top-0 right-5 md:hidden'/>
+                 <img src={tailwind} alt="" className='absolute w-10 rounded-full right-0 bottom-36 md:hidden'/>
+            </div>
+        </div>
+      </div>
+      <div className='absolute top-74 right-10 hidden bg-gray-200 p-4 py-4 md:flex flex-col gap-6 rounded-full'> 
+        <img src={facebook} alt="" className='w-12'/>
+        <img src={instagram} alt="" className='w-12'/>
+        <img src={twitter} alt="" className='w-12'/>
+        <img src={linkedin} alt="" className='w-12'/>
+      </div>
     </section>
+
    
   )
 }
