65a8ee1207887ef5016e6b177185ec1aea6c32e3
UDilakshan <uthayakumardilakshan@gmail.com>
2025-02-26 20:10:35 +0530

    Other changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Hero from './components/Hero'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
 import About from './components/About'
+import ParticleBackground from './components/Particle'
 
 const App = () => {
   return (
     <>
+    <ParticleBackground />
       <Navbar />
       <Hero />
       <About />
